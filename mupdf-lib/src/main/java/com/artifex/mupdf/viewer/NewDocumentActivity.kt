a839fe18d8c8b9ca9e0f126ba6cd61d08832542d
adesa <adesanyaenoch@gmail.com>
2024-10-10 20:19:38 +0100

    Attempting a Compose version of the DocumentViewer

---
+++ mupdf-lib/src/main/java/com/artifex/mupdf/viewer/NewDocumentActivity.kt
@@ -0,0 +1,16 @@
+package com.artifex.mupdf.viewer
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class NewDocumentActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+
+        }
+    }
+}
