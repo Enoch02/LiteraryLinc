0fb0ad5f35083417d2467a24f71e1c4bc29d32b8
Enoch <adesanyaenoch@gmail.com>
2025-03-03 11:16:52 +0100

    feature: excel friendly CSV export

--- features/more/src/main/java/com/enoch02/more/backup_restore/data/BackupRestoreRepository.kt
+++ features/more/src/main/java/com/enoch02/more/backup_restore/data/BackupRestoreRepository.kt
@@ -3,7 +3,7 @@ package com.enoch02.more.backup_restore.data
 import android.net.Uri
 
 interface BackupRestoreRepository {
-    fun createBackup(backupUri: Uri)
+    fun createBackup(backupUri: Uri, excelFriendly: Boolean = false)
 
     fun restoreBackup(backupUri: Uri)
 }
